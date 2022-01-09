0dad5e29c3d1df623bd793f2c1dad82da8b2e9e3
Peruzini <assamitafael@gmail.com>
2022-01-09 18:05:57 -0300

    Adicionando Rogue 2

---
+++ rogue2.js
@@ -0,0 +1,23 @@
+function opcao(escolha) {        
+    if (escolha === 1) {
+        return 1;
+    } 
+    else if(escolha === 2) {
+        return 2;
+    }
+    else{
+        return 3;
+    }   
+}
+
+function filtragem(escolha){
+    while (escolha  > 4 || escolha < 1 ) {
+        alert('OPÇÃO INVALIDA')
+        escolha = Number(prompt());   
+    }
+    return escolha;
+}
+
+function inicio(params) {
+    window.location.href = 'rogue3.html'
+}
